ed5bd34429aabe2adc6a19472f057372065feec5
Jon Lai Lam <jon@lailam.co.za>
2017-12-29 18:12:12 +0200

    Added liveness probe.

--- server/server.js
+++ server/server.js
@@ -6,12 +6,21 @@ const path = require('path');
 
 const port = process.env.PORT || 3701;
 
+const heathCheck = require("./healthCheck");
+
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
   const clientPath = path.resolve(__dirname, '../client/build');
   console.log(`Serving up static content form ${clientPath}`);
   app.use(express.static(clientPath));
 }
+
+app.get('/ping', heathCheck);
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 const server = app.listen(port, () => {
   const port = server.address().port;
